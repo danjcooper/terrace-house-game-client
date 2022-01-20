7d0ddea0988a16333b656ace6c09b312a8284221
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-01-20 19:53:47 +0000

    add season selector component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { SeasonSelector } from './components';
+
+import './style.css';
 
 const App = () => {
-    return <h1>hello react</h1>
-}
+  return (
+    <main>
+      <h1>hello react</h1>
+      <SeasonSelector />
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SeasonSelector/index.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const SeasonSelector = () => {
+  const [seasonsData, setSeasonsData] = useState(null);
+  const [selectedSeasons, setSelectedSeasons] = useState([]);
+
+  useEffect(() => {
+    const getSeasonsData = async () => {
+      const response = await axios.get(
+        'https://terrace-house-server.herokuapp.com/seasons'
+      );
+      setSeasonsData(response.data);
+    };
+    getSeasonsData();
+  }, []);
+
+  const generateSeasonButtons = (data) => {
+    return (
+      <>
+        {data.map((item) => (
+          <label key={item.seasonid} onClick={handleClick}>
+            {item.seasonname}
+            <input type='checkbox' value={item.seasonname} />
+          </label>
+        ))}
+      </>
+    );
+  };
+
+  const toggleSelectedSeasons = (season) => {
+    const seasonAlreadySelected = selectedSeasons.includes(season)
+      ? true
+      : false;
+
+    const newSeasonsArr = selectedSeasons.slice(0);
+
+    if (seasonAlreadySelected) {
+      const indexOfSeason = newSeasonsArr.indexOf(season);
+      newSeasonsArr.splice(indexOfSeason, 1);
+    } else {
+      newSeasonsArr.push(season);
+    }
+    setSelectedSeasons(newSeasonsArr);
+  };
+
+  const handleClick = (e) => {
+    e.target.classList.toggle('active');
+    toggleSelectedSeasons(e.target.closest('input').value);
+  };
+
+  return (
+    <>
+      <h2>Season selector</h2>
+      {seasonsData ? generateSeasonButtons(seasonsData) : <h1>Loading Data</h1>}
+    </>
+  );
+};
+
+export default SeasonSelector;
