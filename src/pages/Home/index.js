95b7ea8e1e16643d78142bcb28782a4a40414834
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-01-22 23:30:20 +0000

    start homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Home = () => {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <h1>TERRACE</h1>
+      <h1>HOUSE</h1>
+      <h1>STATS</h1>
+      <button type='button'>Play</button>
+      <button type='button'>Leaderboard</button>
+      <p>About the game.</p>
+    </>
+  );
 };
 
 export default Home;
